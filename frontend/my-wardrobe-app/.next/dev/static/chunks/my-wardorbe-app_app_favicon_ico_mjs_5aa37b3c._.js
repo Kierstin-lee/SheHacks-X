7e89251e7cd69ad280e4b22f827e81535f90bf1f
etema23 <ertatema7@gmail.com>
2026-01-10 19:18:16 -0500

    Add frontend app (my-wardrobe-app)

---
+++ frontend/my-wardrobe-app/.next/dev/static/chunks/my-wardorbe-app_app_favicon_ico_mjs_5aa37b3c._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/64006_next_dist_600cd1ee._.js"
+],
+    source: "dynamic"
+});
---
+++ frontend/my-wardrobe-app/.next/dev/static/chunks/my-wardorbe-app_app_page_tsx_f51360a6._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/64006_next_dist_0c7bf1e0._.js"
+],
+    source: "dynamic"
+});
